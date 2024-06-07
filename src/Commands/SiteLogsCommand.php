4ccfe37a179ce2d2d76f66c1d732c0647e713cf2
lcatlett <lindsey.catlett@gmail.com>
2024-06-07 07:22:41 -0400

    Recurse into subdirectories when using --all.

--- src/Commands/SiteLogsCommand.php
+++ src/Commands/SiteLogsCommand.php
@@ -150,7 +150,7 @@ class SiteLogsCommand extends TerminusCommand implements SiteAwareInterface
             if ($options['all'])
             {
                 $this->log()->notice('Running {cmd}', ['cmd' => "rsync $rsync_options $src@$app_server_ip:logs/* $dir"]);
-                $this->passthru("rsync $rsync_options -zi --progress --ipv4 --exclude=.git -e 'ssh -p 2222' $src@$app_server_ip:logs/* $dir $devnull");
+                $this->passthru("rsync $rsync_options -zir --progress --ipv4 --exclude=.git -e 'ssh -p 2222' $src@$app_server_ip:logs/* $dir $devnull");
             }
             else
             {
